4a627cc95409f43416bd3683ab78cba5c9b4d695
unknown <nestor.informatica95@gmail.com>
2020-10-15 15:22:41 +0100

    Crud de prueba

---
+++ frontend/app/src/main/java/com/example/bicycles/BicycleListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bicycles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BicycleListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bicycle_list)
+    }
+}
